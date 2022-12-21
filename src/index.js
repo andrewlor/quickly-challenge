e04db487502513cd9470eeaddfe308308ee972ad
Andrew Lor <andrewlor332@gmail.com>
2022-12-20 18:03:09 -0700

    setup tailwind css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 
-const root = createRoot(document.getElementById("root"));
-root.render(<p>Hello World!</p>);
+const root = createRoot(document.getElementById('root'))
+root.render(<p className="text-3xl font-bold underline">Hello World!</p>)
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
+    entry: './src/index.js',
+    mode: 'development',
     module: {
         rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules)/,
-                loader: "babel-loader",
-                options: { presets: ["@babel/env"] },
+                loader: 'babel-loader',
+                options: { presets: ['@babel/env'] },
             },
             {
-                test: /\.sass$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
             },
         ],
     },
-    resolve: { extensions: ["*", ".js", ".jsx"] },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
     output: {
-        path: path.resolve(__dirname, "dist/"),
-        publicPath: "/dist/",
-        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: 'bundle.js',
     },
     devServer: {
         port: 3000,
-        host: "localhost",
+        host: 'localhost',
         hot: true,
     },
     plugins: [new webpack.HotModuleReplacementPlugin()],
-};
+}
